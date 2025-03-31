c7159c3eb4c4ab0e871d2174fd7b01255e32b618
Michael Lyga <31606955+mkwiiallpro@users.noreply.github.com>
2025-03-31 02:34:21 -0400

    multiplayer lobbies can be opened
    All they do is display who is in the game, but it's something.

--- Assets/AirConsole/GameLogic.cs
+++ Assets/AirConsole/GameLogic.cs
@@ -7,14 +7,18 @@ using Newtonsoft.Json.Linq;
 using UnityEngine.Networking;
 public class AirConsoleGameLogic : MonoBehaviour
 {
-
+    public Text names;
     void Awake() {
-        Debug.Log("Hello!");
         AirConsole.instance.onMessage += OnMessage;
+        AirConsole.instance.onConnect += OnConnect;
     }
     void OnMessage(int from, JToken data) {
-        Debug.Log("Button pressed: ",data.ToString());
+        Debug.Log("Button pressed: "+data.ToString());
         AirConsole.instance.Message(from, "Full of pixels!");
+    }
+    void OnConnect(int device_id){
+        string newNickname = AirConsole.instance.GetNickname(device_id);
+        names.text = names.text.Insert(0,newNickname+"\n");
     }
 
     // Update is called once per frame
